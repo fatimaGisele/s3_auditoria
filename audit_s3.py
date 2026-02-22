a80c3f68548ca641aea665e4ff2252d9f605ee62
Altamirano Fatima Gisele <fatima.altamirano@davinci.edu.ar>
2026-02-22 08:44:05 -0300

    Add files via upload

--- audit_s3.py
+++ audit_s3.py
@@ -20,6 +20,11 @@ def audit_s3():
     response = s3.list_buckets()
     buckets = response.get("Buckets",[])
 
+    if not buckets:
+        print("No hay buckets")
+        exit()
+
+
     print("===AUDITORIA===\n")
     for b in buckets:
         name = b["Name"]
@@ -33,6 +38,7 @@ def audit_s3():
 
         print(f"client status: {status}")
 
+
         try:
             lifecycle = s3.get_bucket_lifecycle_configuration(Bucket = name)
             rules = lifecycle.get("Rules", [])
@@ -40,6 +46,35 @@ def audit_s3():
         except ClientError:
             print("rules: NONE")
 
+
+        try:
+            encrypt = s3.get_bucket_encryption(Bucket = name)
+            print(f"Encriptacion habilitada {encrypt}")
+        except ClientError:
+            print("encriptacion no habilitada")
+
+        
+        try:
+            public_access_block = s3.get_public_access_block(Bucket = name)
+            config = public_access_block["PublicAccessBlockConfiguration"]
+            print(f"bloque publico configurado {config}")
+        except ClientError:
+            print("bloque publico no configurado")
+
+
+        try:
+            s3.get_bucket_policy(Bucket = name)
+            print("bucket policy existe")
+        except:
+            print("bucket policy no existe")
+
+        
+        try:
+            objs = s3.list_objects_v2(Bucket = name)
+            count = objs.get("KeyCount", 0)
+            print(f"cantida de objetos: {count}")
+        except:
+            print("no hay objetos")
 
 if __name__ == "__main__":
     audit_s3()
